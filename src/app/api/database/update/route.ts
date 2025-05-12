00a5b53453335666788e56a0a994d05f48a82f61
notwindstone <notwindstone@gmail.com>
2025-05-12 23:35:46 +0300

    feat: implement fully working cache sync with db

--- src/app/api/database/update/route.ts
+++ src/app/api/database/update/route.ts
@@ -2,41 +2,42 @@ import { NextRequest } from "next/server";
 import { LikesCache, RTLCache } from "@/lib/cache";
 import database from "@/db";
 import { likesTable } from "@/db/schema";
-import {getRelativeDate} from "@/utils/getRelativeDate";
+import { getRelativeDate } from "@/utils/getRelativeDate";
 
 export async function GET(request: NextRequest): Promise<Response> {
     const pathname = request.nextUrl.pathname;
 
+    // ratelimit for an hour
     if (RTLCache.has(pathname)) {
         return new Response(null, {
             status: 429,
         });
     }
 
-    LikesCache.set("asdfasdf", {
-        time: getRelativeDate({ minutes: -70 }),
-    });
-    LikesCache.set("asdfdsfsdaf", {
-        time: getRelativeDate({ minutes: -59 }),
-    });
-    LikesCache.set("wr23r23", {
-        time: getRelativeDate({ minutes: 0 }),
-    });
-
+    // we sync every recent entry in the cache
+    // (which age is not older than 60 minutes)
+    const entriesToSync = [];
     const entries = LikesCache.entriesDescending();
     let current = entries.next();
 
     while (current.value !== undefined) {
-        console.log(current);
+        const [userid, { time }] = current.value;
+
+        if (time <= getRelativeDate({ hours: -1 })) {
+            break;
+        }
+
+        entriesToSync.push({
+            userid,
+        });
+
         current = entries.next();
     }
 
     try {
         await database
             .insert(likesTable)
-            .values([
-
-            ]);
+            .values(entriesToSync);
     } catch (error) {
         console.log(error);
 
