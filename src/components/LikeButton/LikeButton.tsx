f34e85d52079e7ccb3583fe738160c9fb53737d9
notwindstone <notwindstone@gmail.com>
2025-05-20 22:00:54 +0300

    feat: save code

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -10,44 +10,49 @@ export default function LikeButton({
     likes: number;
     status: "liked" | null;
 }) {
+    const [isLoading, setIsLoading] = useState(false);
     const [likesData, setLikesData] = useState<{
         count: number;
-        action: "liked" | "disliked" | "nah mate, you already did that" | null;
+        action: "liked" | "disliked" | null;
     }>({
         count: likes,
         action: status,
     });
 
     async function handleClick(action: "like" | "dislike") {
-        const { action: status, count } = await Like({ action });
-
-        if (status === null) {
-            setLikesData({
-                action: "nah mate, you already did that",
-                count,
-            });
-
+        if (isLoading) {
             return;
         }
+
+        setIsLoading(true);
+
+        const { action: status, count } = await Like({ action });
 
         setLikesData({
             action: status,
             count,
         });
+        setIsLoading(false);
 
         return;
     }
 
     return (
         <>
+            <div>{isLoading.toString()}</div>
             <div>{likesData.count}</div>
             <div>{likesData.action}</div>
-            <button onClick={() => handleClick("like")}>
-                like?
-            </button>
-            <button onClick={() => handleClick("dislike")}>
-                dislike?
-            </button>
+            {
+                likesData.action === "liked" ? (
+                    <button onClick={() => handleClick("dislike")}>
+                        dislike?
+                    </button>
+                ) : (
+                    <button onClick={() => handleClick("like")}>
+                        like?
+                    </button>
+                )
+            }
         </>
     );
 }
