ae352c2cda6bba0704be2528311097dade0cb6d0
notwindstone <notwindstone@gmail.com>
2025-05-20 23:50:33 +0300

    feat: add initial home page layout

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,17 +1,9 @@
-import FetchLikes from "@/components/FetchLikes/FetchLikes";
-import { Suspense } from "react";
+import FetchLikesWrapper from "@/components/FetchLikesWrapper/FetchLikesWrapper";
 
 export default function Home() {
     return (
         <>
-            <Suspense fallback={
-                <div className="flex flex-nowrap gap-2">
-                    <div className="w-20 h-9 rounded-md bg-neutral-800 animate-pulse" />
-                    <div className="w-9 h-9 rounded-md bg-neutral-800 animate-pulse" />
-                </div>
-            }>
-                <FetchLikes />
-            </Suspense>
+            <FetchLikesWrapper />
         </>
     );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+    return (
+        <>
+            <div className="flex gap-2">
+                <div></div>
+                <div className="flex flex-col">
+                    <div className="flex">
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </>
+    );
+}
