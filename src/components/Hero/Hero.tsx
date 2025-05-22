d0ce5db3f5fc39df1384e6f41c322af3bcb9f242
notwindstone <notwindstone@gmail.com>
2025-05-22 11:35:29 +0300

    feat: change hero layout

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -14,66 +14,74 @@ export default function Hero({
     return (
         <>
             <div className="p-8 w-full h-fit max-w-324 flex flex-wrap gap-4 flex-col lg:flex-row">
-                <div className="flex-1 flex flex-col gap-4 p-8 rounded-3xl lg:rounded-l-3xl lg:rounded-r-lg bg-neutral-950">
-                    <div className="flex flex-wrap sm:flex-nowrap gap-4">
-                        <ConfiguredImage
-                            src={Favicon}
-                            alt="Kaeeraa's profile picture"
-                            className="mx-auto h-24 w-24 object-cover shrink-0 box-content rounded-full p-0.5 [background:linear-gradient(45deg,theme(colors.neutral.900))_padding-box,conic-gradient(from_var(--border-angle),theme(colors.neutral.600/.48)_60%,_theme(colors.neutral.300)_72%,_theme(colors.neutral.100)_80%,_theme(colors.neutral.300)_88%,_theme(colors.neutral.600/.48))_border-box] border-2 border-transparent animate-border transition"
-                        />
-                        <div className="shrink-0 flex-1 flex flex-col gap-2 justify-center">
-                            <p className="font-medium leading-none text-lg">
-                                <Translate property={"components.hero.title"} />
-                            </p>
-                            <p className="text-neutral-400 leading-none">
-                                <Translate property={"components.hero.description"} />
-                            </p>
-                            <div className="flex gap-2">
+                <div className="flex-1 min-h-106 flex flex-col justify-between gap-4 p-8 rounded-3xl lg:rounded-l-3xl lg:rounded-r-lg bg-neutral-950">
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-wrap sm:flex-nowrap gap-4">
+                            <ConfiguredImage
+                                src={Favicon}
+                                alt="Kaeeraa's profile picture"
+                                className="mx-auto h-24 w-24 object-cover shrink-0 box-content rounded-full p-0.5 [background:linear-gradient(45deg,theme(colors.neutral.900))_padding-box,conic-gradient(from_var(--border-angle),theme(colors.neutral.600/.48)_60%,_theme(colors.neutral.300)_72%,_theme(colors.neutral.100)_80%,_theme(colors.neutral.300)_88%,_theme(colors.neutral.600/.48))_border-box] border-2 border-transparent animate-border transition"
+                            />
+                            <div className="shrink-0 flex-1 flex flex-col gap-2 justify-center">
+                                <p className="font-medium leading-none text-lg">
+                                    <Translate property={"components.hero.title"} />
+                                </p>
+                                <p className="text-neutral-400 leading-none">
+                                    <Translate property={"components.hero.description"} />
+                                </p>
+                                <div className="flex gap-2">
+                                    {
+                                        ProgrammingLanguages.map((name) => {
+                                            return (
+                                                <div
+                                                    key={name}
+                                                    className="bg-neutral-900 w-fit px-2 py-1 text-sm rounded-full"
+                                                >
+                                                    {name}
+                                                </div>
+                                            );
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <ul>
                                 {
-                                    ProgrammingLanguages.map((name) => {
+                                    AboutMe.map((about) => {
                                         return (
-                                            <div
-                                                key={name}
-                                                className="bg-neutral-900 w-fit px-2 py-1 text-sm rounded-full"
-                                            >
-                                                {name}
+                                            <div key={about} className="flex flex-nowrap items-center gap-4 mt-4">
+                                                <p className="leading-none">
+                                                    ✦
+                                                </p>
+                                                <p className="leading-none text-pretty text-neutral-400">
+                                                    <Translate property={about} />
+                                                </p>
                                             </div>
                                         );
                                     })
                                 }
-                            </div>
+                            </ul>
                         </div>
                     </div>
-                    <div>
-                        <ul>
-                            {
-                                AboutMe.map((about) => {
-                                    return (
-                                        <div key={about} className="flex flex-nowrap items-center gap-4 mt-4">
-                                            <p className="leading-none">
-                                                ✦
-                                            </p>
-                                            <p className="leading-none text-pretty text-neutral-400">
-                                                <Translate property={about} />
-                                            </p>
-                                        </div>
-                                    );
-                                })
-                            }
-                        </ul>
-                    </div>
-                    <div className="flex justify-end gap-2">
+                    <div className="flex items-end justify-end gap-2">
                         <LocaleSwitcher />
                         <FetchLikesWrapper lang={lang} />
                     </div>
                 </div>
-                <div className="flex-1 p-8 rounded-3xl lg:rounded-r-3xl lg:rounded-l-lg bg-neutral-950">
-                    <p className="font-medium text-lg">
-                        Hi! I&#39;m Kaeeraa
-                    </p>
-                    <p className="text-neutral-400">
-                        Problem-solver at heart, passionate about Linux (NixOS) and open-source apps.
-                    </p>
+                <div className="flex-1 flex flex-col gap-4">
+                    <div className="p-8 h-full rounded-3xl lg:rounded-r-3xl lg:rounded-l-lg bg-neutral-950">
+                        <p className="font-medium text-lg">
+                            <Translate property={"components.hero.projects.title"} />
+                        </p>
+
+                    </div>
+                    <div className="p-8 shrink-0 rounded-3xl lg:rounded-r-3xl lg:rounded-l-lg bg-neutral-950">
+                        <p className="font-medium text-lg">
+                            <Translate property={"components.hero.socials.title"} />
+                        </p>
+
+                    </div>
                 </div>
             </div>
         </>
